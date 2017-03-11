033088a3b88dbca99a2d7cafacbb77a467629656
Andrew Nguyen <Andrew Nguyen>
2017-03-11 15:41:12 -0700

    Updated Login features 2.0

--- routes/users.js
+++ routes/users.js
@@ -6,22 +6,58 @@ var appSecret = process.env.NODE_APP_SECRET;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  if (!req.cookies.userID){
+    return res.redirect('/users/login');
+  }
+
   var User = mongoose.model('User', userSchema);
-  User.find({}).exec(function(err, users){
+  User.find({})
+    .exec(function(err, users){
     if (err) throw err;
 
     res.status(200).json({users: users});
   });
-  res.send('respond with a resource');
 });
 
-// GET logins
-router.get('/login',function(req,res,next){
-  res.render('users/login');
+// GET login
+router.get('/login', function(req, res, next){
+  var notice = req.cookies.notice;
+
+  res.render('users/login', {
+    notice: notice
+  });
 });
 
 // POST logins
-router.post('/login',function(req,res,next){});
+router.post('/login',function(req,res,next){
+  var User = mongoose.model('User', userSchema);
+  var email = req.parm('email');
+  var password = req.param('password');
+  var encryptedPassword = crypto.createHmac('sha256',appSecret).update(password).digest('hex');
+  User.findOne({
+    email: email,
+    encryptedPassword: encryptedPassword
+  })
+  .exec(function(err, user){
+    if (err) throw err;
+
+    // email and password does not match
+    if (!user){
+      console.log('Email and password does not match');
+      res.cookie('notice', 'Email and password does not match', {
+        maxAge: new Date(Date.now() + 10)
+      });
+      return res.redirect('/users/login');
+    }
+      else {
+        res.cookie('userID', user._id, {
+          maxAge: new Date(Date.now() + 10)
+        });
+        return res.redirect('/users');
+      }
+  });
+
+});
 
 // GET register
 router.get('/signup',function(req,res,next){
@@ -33,7 +69,7 @@ router.get('/signup',function(req,res,next){
 
 // POST register
 router.post('/signup',function(req,res,next){
-  var User = mongoose('User', userSchema);
+  var User = mongoose.model('User', userSchema);
   var email = req.param('email');
   var password = req.param('password');
   var passwordAgain = req.param('passwordAgain');
@@ -41,40 +77,53 @@ router.post('/signup',function(req,res,next){
   var lastname = req.param('lastname');
   var university = req.param('university');
 
-  // check if password match
-  if (password != passwordAgain){
-    console.log('Password do not match');
-    res.cookie('notice', 'Passwords do not match');
+  // check if empty
+  if (!password || !email){
+    console.log('Empty fields.');
+    res.cookie('notice', 'Please fill in al the fields correctly', {
+      maxAge: new Date(Date.now() + 10)
+    });
 
-    return res.redirect('/users/signup')
+    return res.redirect('/users/signup');
+  }
+
+  // check if passwords match
+  if (password !== passwordAgain){
+    console.log('Passwords does not match');
+    res.cookie('notice', 'Passwords does not match', {
+      maxAge: new Date(Date.now() + 10)
+    });
+
+    return res.redirect('/users/signup');
   }
 
   // check if user exists
   User.findOne({
     email: email
   })
-    .exec(function(err,user){
-      if (err) throw err;
+  .exec(function(err, user){
+    if (err) throw err;
 
-      if (user){
-        console.log('User exists');
-        res('notice', 'User exists', {
-          maxAge: new Date(Date.now() + 10),
-        });
-        return res.redirect('/users/signup');
-      } else {
-        // encrypt the password
-        var encryptedPassword = crypto.createHmac('sha256', appSecret).update(password).digest('hex');
-
-        // create new user
+    if (user){
+      console.log('User exists');
+      res.cookie('notice', 'User exists', {
+        maxAge: new Date(Date.now() + 10)
+      });
+      return res.redirect('/users/signup');
+    }
+      else{
+        // encryptd the password
+        var encryptedPassword = crypto.createHmac('sha256',appSecret)
+                  .update(password).digest('hex');
+        // create a new user
         var user = new User({
           email: email,
-          password: encryptedPassword
+          encryptedPassword: encryptedPassword
         });
         user.save(function(err){
           if (err) throw err;
           res.cookie('notice', 'Successfully registered', {
-            maxAge: new Date(Date.now() + 10),
+            maxAge: new Date(Date.now() + 10)
           });
 
           return res.redirect('/users/login');
@@ -82,11 +131,11 @@ router.post('/signup',function(req,res,next){
       }
     });
 
-  res.status(200).json({email: email,
-    password: password,
-    firstname: firstname,
-    lastname: lastname
   });
+
+router.get('/logout', function(req, res, next){
+  res.clearCookie('userID');
+  res.redirect('/users/login');
 });
 
 module.exports = router;
