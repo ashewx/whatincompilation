b07c4824509f1c978213b91b5a6af786105799e2
Andrew Nguyen <Andrew Nguyen>
2017-03-11 10:23:11 -0700

    Create basic login

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// GET logins
+router.get('/login',function(req,res,next){
+  res.render('users/login');
+});
+
+// POST logins
+router.post('/login',function(req,res,next){});
+
+// GET register
+router.get('/signup',function(req,res,next){});
+
+// POST register
+router.post('/signup',function(req,res,next){});
+
 module.exports = router;
